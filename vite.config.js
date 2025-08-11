28dc177d8bd5317681949579c1c08e1556de10fa
ckimball17 <conradmkimball@gmail.com>
2025-08-11 08:38:34 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 replace with your repo name exactly
+const repoName = 'compressor-tracker' 
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: `/${repoName}/`,
 })
+
